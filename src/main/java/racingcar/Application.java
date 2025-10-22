5e4bd93fedfd9d95b2ff6ab120f39a57cfc3838b
Leeka99 <dsrn0000@gmail.com>
2025-10-22 13:53:32 +0900

    feat: 시도할 횟수 저장 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -14,5 +14,6 @@ public class Application {
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         String inputCount = Console.readLine();
+        int count = Integer.parseInt(inputCount);
     }
 }
