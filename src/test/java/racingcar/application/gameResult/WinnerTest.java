80e821112527df996acedeee632fff394f9a9558
Leeka99 <dsrn0000@gmail.com>
2025-10-24 20:42:03 +0900

    test: 단독 우승자, 공동 우승자 테스트 분리

--- src/test/java/racingcar/application/gameResult/WinnerTest.java
+++ src/test/java/racingcar/application/gameResult/WinnerTest.java
@@ -12,29 +12,56 @@ public class WinnerTest {
     Winner winner;
 
     @Test
-    @DisplayName("[success] 단독 우승자 테스트")
-    void singleWinner() {
+    @DisplayName("[success] 단독 우승자 인원 수 반환 테스트")
+    void singleWinnerNumberTest() {
         List<Integer> gameResult = List.of(3, 1, 2);
         int maxNumber = 3;
         winner = new Winner(carNames, gameResult, maxNumber);
 
         int winnerNumber = winner.calculateFinalResult();
 
-        Assertions.assertThat(winnerNumber).isEqualTo(1);
-        Assertions.assertThat(winner.getWinner()).isEqualTo(List.of("pobi"));
+        Assertions.assertThat(winnerNumber)
+            .isEqualTo(1);
     }
 
     @Test
-    @DisplayName("[success] 공동 우승자 테스트")
-    void multiWinner() {
+    @DisplayName("[success] 단독 우승자 이름 반환 테스트")
+    void singleWinnerNameTest() {
+        List<Integer> gameResult = List.of(3, 1, 2);
+        int maxNumber = 3;
+        winner = new Winner(carNames, gameResult, maxNumber);
+
+        winner.calculateFinalResult();
+        List<String> winnerValue = winner.getWinner();
+
+        Assertions.assertThat(winnerValue)
+            .isEqualTo(List.of("pobi"));
+    }
+
+    @Test
+    @DisplayName("[success] 공동 우승자 인원 수 반환 테스트")
+    void multiWinnerNumberTest() {
         List<Integer> gameResult = List.of(1, 2, 2);
         int maxNumber = 2;
         winner = new Winner(carNames, gameResult, maxNumber);
 
         int winnerNumber = winner.calculateFinalResult();
 
-        Assertions.assertThat(winnerNumber).isEqualTo(2);
-        Assertions.assertThat(winner.getWinner()).isEqualTo(List.of("lisa", "poo"));
+        Assertions.assertThat(winnerNumber)
+            .isEqualTo(2);
     }
 
+    @Test
+    @DisplayName("[success] 공동 우승자 이름 반환 테스트")
+    void multiWinnerNameTest() {
+        List<Integer> gameResult = List.of(1, 2, 2);
+        int maxNumber = 2;
+        winner = new Winner(carNames, gameResult, maxNumber);
+
+        winner.calculateFinalResult();
+        List<String> winnerValue = winner.getWinner();
+
+        Assertions.assertThat(winnerValue)
+            .isEqualTo(List.of("lisa", "poo"));
+    }
 }
