833df9088c2f1619ce73293aaf4e61044603b010
Leeka99 <dsrn0000@gmail.com>
2025-10-24 20:38:47 +0900

    test: 0을 포함한 숫자(ex: 03) 형식의 입력 테스트 구현

--- src/test/java/racingcar/application/input/InputCountTest.java
+++ src/test/java/racingcar/application/input/InputCountTest.java
@@ -72,4 +72,12 @@ public class InputCountTest {
         Assertions.assertThatThrownBy(() -> countInput.inputCount(count))
             .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    @DisplayName("[success] 0을 포함한 숫자(ex: 03) 형식의 입력 테스트")
+    void inputWithZeroTest(){
+        String count = "03";
+        Assertions.assertThat(countInput.inputCount(count))
+            .isEqualTo(3);
+    }
 }
