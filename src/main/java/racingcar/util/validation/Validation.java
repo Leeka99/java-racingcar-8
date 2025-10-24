4363576911e705665cd16c6fab1c0428d0ee6a17
Leeka99 <dsrn0000@gmail.com>
2025-10-24 16:20:49 +0900

    refactor: Application에서 예외처리에 대해서 util으로 분리

---
+++ src/main/java/racingcar/util/validation/Validation.java
@@ -0,0 +1,33 @@
+package racingcar.util.validation;
+
+public class Validation {
+
+    public static void validBlankNames(String inputCarNames) {
+        if (inputCarNames.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validInputName(String name) {
+        if (5 < name.length()) {
+            throw new IllegalArgumentException();
+        }
+        if (name.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static String validCount(String inputCount) {
+        if (inputCount.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        if (!inputCount.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException();
+        }
+        if (Integer.parseInt(inputCount) < 1) {
+            throw new IllegalArgumentException();
+        }
+        return inputCount;
+    }
+
+}
