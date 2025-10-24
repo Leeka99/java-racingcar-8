81a6e443fc9b80cf3bac0c99ad96f1d449c4ed57
Leeka99 <dsrn0000@gmail.com>
2025-10-24 16:22:53 +0900

    test: 횟수 입력 테스트 구조 변경

--- src/test/java/racingcar/application/input/InputCountTest.java
+++ src/test/java/racingcar/application/input/InputCountTest.java
@@ -3,17 +3,17 @@ package racingcar.application.input;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.Application;
+import racingcar.view.input.CountInput;
 
-public class inputCountTest {
+public class InputCountTest {
 
-    Application raceGame = new Application();
+    CountInput countInput = new CountInput();
 
     @Test
     @DisplayName("[fail] 공백 입력 테스트")
     void blankTest() {
         String count = " ";
-        Assertions.assertThatThrownBy(() -> raceGame.inputCount(count))
+        Assertions.assertThatThrownBy(() -> countInput.inputCount(count))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -21,7 +21,7 @@ public class inputCountTest {
     @DisplayName("[fail] 문자 입력 테스트")
     void stringTest() {
         String count = "횟수는 5";
-        Assertions.assertThatThrownBy(() -> raceGame.inputCount(count))
+        Assertions.assertThatThrownBy(() -> countInput.inputCount(count))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -29,14 +29,14 @@ public class inputCountTest {
     @DisplayName("[success] 공백 포함 입력 테스트")
     void withBlankTest() {
         String count = "5   ";
-        Assertions.assertThat(raceGame.inputCount(count)).isEqualTo(5);
+        Assertions.assertThat(countInput.inputCount(count)).isEqualTo(5);
     }
 
     @Test
     @DisplayName("[fail] 0 입력 테스트")
     void inputZero() {
         String count = "0";
-        Assertions.assertThatThrownBy(() -> raceGame.inputCount(count))
+        Assertions.assertThatThrownBy(() -> countInput.inputCount(count))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -44,7 +44,7 @@ public class inputCountTest {
     @DisplayName("[fail] 음수 입력 테스트")
     void inputMinus() {
         String count = "-7";
-        Assertions.assertThatThrownBy(() -> raceGame.inputCount(count))
+        Assertions.assertThatThrownBy(() -> countInput.inputCount(count))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -52,7 +52,7 @@ public class inputCountTest {
     @DisplayName("[success] 정상 입력 테스트")
     void inputCountTest() {
         String count = "10";
-        Assertions.assertThat(raceGame.inputCount(count)).isEqualTo(10);
+        Assertions.assertThat(countInput.inputCount(count)).isEqualTo(10);
     }
 
 }
