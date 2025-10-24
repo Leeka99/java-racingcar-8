ae1a97f514f1d183905cf34ac74e891a827af241
Leeka99 <dsrn0000@gmail.com>
2025-10-24 19:23:37 +0900

    test: 횟수 입력에 대한 테스트 로직 추가

--- src/test/java/racingcar/application/input/InputCountTest.java
+++ src/test/java/racingcar/application/input/InputCountTest.java
@@ -10,7 +10,7 @@ public class InputCountTest {
     CountInput countInput = new CountInput();
 
     @Test
-    @DisplayName("[fail] 공백 입력 테스트")
+    @DisplayName("[fail] 공백만 입력 테스트")
     void blankTest() {
         String count = " ";
         Assertions.assertThatThrownBy(() -> countInput.inputCount(count))
@@ -18,9 +18,17 @@ public class InputCountTest {
     }
 
     @Test
+    @DisplayName("[fail] 문자가 포함된 입력 테스트")
+    void withStringTest() {
+        String count = "횟수는 5";
+        Assertions.assertThatThrownBy(() -> countInput.inputCount(count))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
     @DisplayName("[fail] 문자 입력 테스트")
     void stringTest() {
-        String count = "횟수는 5";
+        String count = "우아한테크코스!";
         Assertions.assertThatThrownBy(() -> countInput.inputCount(count))
             .isInstanceOf(IllegalArgumentException.class);
     }
@@ -29,12 +37,13 @@ public class InputCountTest {
     @DisplayName("[success] 공백 포함 입력 테스트")
     void withBlankTest() {
         String count = "5   ";
-        Assertions.assertThat(countInput.inputCount(count)).isEqualTo(5);
+        Assertions.assertThat(countInput.inputCount(count))
+            .isEqualTo(5);
     }
 
     @Test
     @DisplayName("[fail] 0 입력 테스트")
-    void inputZero() {
+    void inputZeroTest() {
         String count = "0";
         Assertions.assertThatThrownBy(() -> countInput.inputCount(count))
             .isInstanceOf(IllegalArgumentException.class);
@@ -42,7 +51,7 @@ public class InputCountTest {
 
     @Test
     @DisplayName("[fail] 음수 입력 테스트")
-    void inputMinus() {
+    void inputMinusTest() {
         String count = "-7";
         Assertions.assertThatThrownBy(() -> countInput.inputCount(count))
             .isInstanceOf(IllegalArgumentException.class);
@@ -52,7 +61,15 @@ public class InputCountTest {
     @DisplayName("[success] 정상 입력 테스트")
     void inputCountTest() {
         String count = "10";
-        Assertions.assertThat(countInput.inputCount(count)).isEqualTo(10);
+        Assertions.assertThat(countInput.inputCount(count))
+            .isEqualTo(10);
     }
 
+    @Test
+    @DisplayName("[fail] 소수점이 포함된 숫자 입력 테스트")
+    void inputDecimalNumberTest() {
+        String count = "3.5";
+        Assertions.assertThatThrownBy(() -> countInput.inputCount(count))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
 }
