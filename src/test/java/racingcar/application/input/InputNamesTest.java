6f39610ca59677dd8ee842a03ec9d17e8fd3a8d1
Leeka99 <dsrn0000@gmail.com>
2025-10-24 16:22:33 +0900

    test: 자동차 이름 입력 테스트 구조 변경

--- src/test/java/racingcar/application/input/InputNamesTest.java
+++ src/test/java/racingcar/application/input/InputNamesTest.java
@@ -4,17 +4,17 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.Application;
+import racingcar.view.input.CarNameInput;
 
-public class inputNamesTest {
+public class InputNamesTest {
 
-    Application raceGame = new Application();
+    CarNameInput carNameInput = new CarNameInput();
 
     @Test
     @DisplayName("[fail] 공백 입력 테스트")
     void blankTest() {
         String inputCarNames = "";
-        Assertions.assertThatThrownBy(() -> raceGame.inputNames(inputCarNames))
+        Assertions.assertThatThrownBy(() -> carNameInput.inputNames(inputCarNames))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -22,7 +22,7 @@ public class inputNamesTest {
     @DisplayName("[fail] 5글자 초과 입력 테스트")
     void LenthTest() {
         String carName = "Leeka99";
-        Assertions.assertThatThrownBy(() -> raceGame.inputNames(carName))
+        Assertions.assertThatThrownBy(() -> carNameInput.inputNames(carName))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -30,7 +30,7 @@ public class inputNamesTest {
     @DisplayName("[fail] 숫자 입력 테스트")
     void DigitTest() {
         String carName = "8";
-        Assertions.assertThatThrownBy(() -> raceGame.inputNames(carName))
+        Assertions.assertThatThrownBy(() -> carNameInput.inputNames(carName))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -38,7 +38,7 @@ public class inputNamesTest {
     @DisplayName("[success] (,) 쉼표 기준으로 분리 테스트")
     void seperateTest() {
         String carName = "Leeka,pobi.,*jini";
-        Assertions.assertThat(raceGame.inputNames(carName))
+        Assertions.assertThat(carNameInput.inputNames(carName))
             .isEqualTo(List.of("Leeka", "pobi.", "*jini"));
     }
 
@@ -46,7 +46,7 @@ public class inputNamesTest {
     @DisplayName("[success] 자동차 이름 정상 입력 테스트")
     void inputNamesTest() {
         String carName = "pobi,lisa,jin";
-        Assertions.assertThat(raceGame.inputNames(carName))
+        Assertions.assertThat(carNameInput.inputNames(carName))
             .isEqualTo(List.of("pobi", "lisa", "jin"));
     }
 
@@ -54,7 +54,7 @@ public class inputNamesTest {
     @DisplayName("[success] 자동차 이름 공백 포함 입력 테스트")
     void inputNamesBlankTest() {
         String carName = "Leeka , pobi.,*jini";
-        Assertions.assertThat(raceGame.inputNames(carName))
+        Assertions.assertThat(carNameInput.inputNames(carName))
             .isEqualTo(List.of("Leeka", "pobi.", "*jini"));
     }
 }
